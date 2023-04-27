b57f876885687a5f5e3c48611afb27627cf85ffd
emilio <emilio.lihue.q@gmail.com>
2023-04-26 22:33:11 -0300

    update

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/css/index.scss';
 import App from './components/App';
-// todo habilitar redux, crear store y aplicar el middleware  de saga
+// todo habilitar redux, crear store y aplicar el middleware  de redux saga
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
