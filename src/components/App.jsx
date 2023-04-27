bb854a84fad935526fe07fd34d1e68b01a785dea
emilio <emilio.lihue.q@gmail.com>
2023-04-26 22:05:25 -0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * @returns {React.Component} Componente Principal de la aplicacion
+ */
+// eslint-disable-next-line react/function-component-definition
+const App = () => {
+  return (
+    <div>
+      <h1>hola mundo</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/css/index.scss';
+import App from './components/App';
+// todo habilitar redux, crear store y aplicar el middleware  de saga
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
